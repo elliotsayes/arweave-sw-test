9b5e9c080a4c921222d6861cd4c78d99352a0481
Elliot Sayes <elliot@sayes.cloud>
2023-11-24 13:51:27 +0700

    Allow module imports in dev

--- src/loadSw.ts
+++ src/loadSw.ts
@@ -2,14 +2,18 @@ export const loadSw = () => {
   if ("serviceWorker" in navigator) {
     const swName =
       import.meta.env.MODE === "production" ? "/sw.js" : "/dev-sw.js?dev-sw";
-    navigator.serviceWorker.register(swName).then(
-      (registration) => {
-        console.log("Registered a Service Worker ", registration);
-      },
-      (error) => {
-        console.error("Could not register a Service Worker ", error);
-      }
-    );
+    navigator.serviceWorker
+      .register(swName, {
+        type: import.meta.env.MODE === "production" ? "classic" : "module",
+      })
+      .then(
+        (registration) => {
+          console.log("Registered a Service Worker ", registration);
+        },
+        (error) => {
+          console.error("Could not register a Service Worker ", error);
+        }
+      );
   } else {
     console.error("Service Workers are not supported in this browser.");
   }
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       },
       devOptions: {
         enabled: true,
+        type: "module",
       },
     }),
   ],

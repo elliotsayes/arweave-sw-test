f964e8fc8a76c66ee5422f69ff28fa5277c8473c
Elliot Sayes <elliot@sayes.cloud>
2023-11-24 11:29:30 +0700

    Attempt to intercept ar:// request Fails due to net::ERR_UNKNOWN_URL_SCHEME

--- src/sw.ts
+++ src/sw.ts
@@ -22,6 +22,18 @@ self.addEventListener("fetch", (e) => {
   console.log(`[Service Worker] Fetch ${event.request.url}`);
   event.respondWith(
     (async () => {
+      const url = event.request.url;
+      const arRegex = /^ar:\/\/([a-zA-Z0-9_-]{43})$/;
+      let m: RegExpExecArray | null;
+      if ((m = arRegex.exec(url)) !== null) {
+        console.log("ar:// URL detected", url);
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+          console.log(`Found match, group ${groupIndex}: ${match}`);
+        });
+      } else {
+        console.log("Base URL detected", url);
+      }
       return await fetch(event.request.url);
     })()
   );
